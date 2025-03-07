5c5cb7b0f0e37cd23eea81baae4613114e7e8b1f
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2025-03-08 01:25:44 +0600

    refractoring

--- vlmeval/dataset/image_mcq.py
+++ vlmeval/dataset/image_mcq.py
@@ -93,7 +93,7 @@ class ImageMCQDataset(ImageBaseDataset):
         'A-OKVQA': 'https://huggingface.co/datasets/Allen8/A-OKVQA/resolve/main/a-okvqa.tsv',
 
         # Meme-Bench
-        'meme-bench': 'https://huggingface.co/datasets/pltops/humor-bench/resolve/main/humor-bench.tsv',
+        'humorbench': 'https://huggingface.co/datasets/pltops/humorbench/resolve/main/humor-bench.tsv',
     }
 
     DATASET_MD5 = {
